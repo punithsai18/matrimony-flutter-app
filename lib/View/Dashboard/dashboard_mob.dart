b6efe23f2c5585b11500e586cb6266a5784597f6
HK2184 <sharikrishnaiyer2006@gmail.com>
2025-09-07 21:24:51 +0530

    modified

--- lib/View/Dashboard/dashboard_mob.dart
+++ lib/View/Dashboard/dashboard_mob.dart
@@ -49,184 +49,192 @@ class _DashboardMobState extends State<DashboardMob> {
         preferredSize: Size.fromHeight(80),
         child: Header(),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 16),
-              Text(
-                'Find your perfect',
-                style: GoogleFonts.lato(
-                  fontSize: 24,
-                  color: Colors.black,
-                ),
-              ),
-              Text(
-                'Match!',
-                style: GoogleFonts.kodchasan(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.redAccent,
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
-                  hintText: 'Search for partner',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ChoiceChip(label: Text('Age'), selected: false),
-                  ChoiceChip(label: Text('Mothertongue'), selected: false),
-                  ChoiceChip(label: Text('Religion'), selected: false),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildSectionTitle('Near You'),
-                  _viewAllButton(),
-                ],
-              ),
-
-              SizedBox(height: 16),
-              Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                elevation: 4,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16),
-                        topRight: Radius.circular(16),
-                      ),
-                      child: Image.asset(
-                        'assets/images/download.jpg',
-                        height: 200,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'James',
-                            style: GoogleFonts.lato(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            'India, Delhi',
-                            style: GoogleFonts.lato(
-                              fontSize: 14,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            'Chartered Accountant',
-                            style: GoogleFonts.lato(
-                              fontSize: 14,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              SizedBox(height: 2.1.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildSectionTitle(
-                      'New Profiles'), // Use the custom function here
-                  _viewAllButton(),
-                ],
-              ),
-              NewProfileTab(bodyContext: context),
-              SizedBox(height: 3.0.h),
-
-              // Recent Success Section
-              SizedBox(height: 2.0.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildSectionTitle(
-                      'Recent Success'), // Use the custom function here
-                  _viewAllButton(),
-                ],
-              ),
-              RecentSuccess(userList: userList),
-              SizedBox(height: 3.0.h),
-
-              // Profile List Section
-              SizedBox(height: 2.0.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildSectionTitle(
-                      'Profile List'), // Use the custom function here
-                  _viewAllButton(),
-                ],
-              ),
-
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                      minWidth: MediaQuery.of(context).size.width),
-                  child: ProfileListTab(),
-                ),
-              ),
-              SizedBox(height: 3.0.h),
-
-              // Profile Status Section
-              SizedBox(height: 2.0.h),
-              _buildSectionTitle(
-                  'Profile Status'), // Use the custom function here
-              Column(
-                children: [
-                  ProfileStatus(),
-                  SizedBox(height: 1.0.h),
-                  ProfileCompletionStatus(),
-                ],
-              ),
-              SizedBox(height: 5.0.h),
-
-              SizedBox(height: 2.0.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildSectionTitle('Discover Matches'),
-                  _viewAllButton(), // Optional: you can hook this to navigate to full screen
-                ],
-              ),
-              DiscoverSection(), // 👈 Here’s your embedded Discover cards
-            ],
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/bg.jpg'),
+              fit: BoxFit.cover,
+              filterQuality: FilterQuality.high,
+            ),
           ),
-        ),
-      ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 16),
+                  Text(
+                    'Find your perfect',
+                    style: GoogleFonts.lato(
+                      fontSize: 24,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    'Match!',
+                    style: GoogleFonts.kodchasan(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Search for partner',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ChoiceChip(label: Text('Age'), selected: false),
+                      ChoiceChip(label: Text('Mothertongue'), selected: false),
+                      ChoiceChip(label: Text('Religion'), selected: false),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildSectionTitle('Near You'),
+                      _viewAllButton(),
+                    ],
+                  ),
+
+                  SizedBox(height: 16),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 4,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            topRight: Radius.circular(16),
+                          ),
+                          child: Image.asset(
+                            'assets/images/download.jpg',
+                            height: 200,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'James',
+                                style: GoogleFonts.lato(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                'India, Delhi',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                'Chartered Accountant',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 2.1.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildSectionTitle(
+                          'New Profiles'), // Use the custom function here
+                      _viewAllButton(),
+                    ],
+                  ),
+                  NewProfileTab(bodyContext: context),
+                  SizedBox(height: 3.0.h),
+
+                  // Recent Success Section
+                  SizedBox(height: 2.0.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildSectionTitle(
+                          'Recent Success'), // Use the custom function here
+                      _viewAllButton(),
+                    ],
+                  ),
+                  RecentSuccess(userList: userList),
+                  SizedBox(height: 3.0.h),
+
+                  // Profile List Section
+                  SizedBox(height: 2.0.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildSectionTitle(
+                          'Profile List'), // Use the custom function here
+                      _viewAllButton(),
+                    ],
+                  ),
+
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                          minWidth: MediaQuery.of(context).size.width),
+                      child: ProfileListTab(),
+                    ),
+                  ),
+                  SizedBox(height: 3.0.h),
+
+                  // Profile Status Section
+                  SizedBox(height: 2.0.h),
+                  _buildSectionTitle(
+                      'Profile Status'), // Use the custom function here
+                  Column(
+                    children: [
+                      ProfileStatus(),
+                      SizedBox(height: 1.0.h),
+                      ProfileCompletionStatus(),
+                    ],
+                  ),
+                  SizedBox(height: 5.0.h),
+
+                  SizedBox(height: 2.0.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildSectionTitle('Discover Matches'),
+                      _viewAllButton(), // Optional: you can hook this to navigate to full screen
+                    ],
+                  ),
+                  DiscoverSection(), // 👈 Here’s your embedded Discover cards
+                ],
+              ),
+            ),
+          )),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex, // Track the selected index
         onTap: (index) {
